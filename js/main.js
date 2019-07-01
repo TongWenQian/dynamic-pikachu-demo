920262e0d6e6edf6b38543ea8e358a3d103b3a61
tongwenqian <839035680@qq.com>
2019-07-01 17:32:48 +0800

    提交

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
 ! function () {
+    var duration = 50;
+    $('.actions').on('click','button',function(e){
+        let $button = $(e.currentTarget)
+        let speed = $button.attr('data-speed')
+        $button.addClass('active')
+        .siblings('.active').removeClass('active')
+        switch(speed){
+            case 'slow':
+                duration = 100
+                break
+            case 'normal':
+                duration = 50
+                break
+            case 'fast':
+                duration = 10
+                break
+        }
+    })
     function writeCode(prefix, code, fn) {
         let container = document.querySelector('#code')
         let styleTag = document.querySelector('#styleTag')
         let n = 0
-        let id = setInterval(() => {
+        setTimeout(function run(){
             n += 1
             container.innerHTML = code.substring(0, n)
             styleTag.innerHTML = code.substring(0, n)
             container.scrollTop = container.scrollHeight
-            if (n >= code.length) {
-                window.clearInterval(id)
+            if (n < code.length) {
+                setTimeout(run,duration)
+            }else{
                 fn && fn.call()
+                window.clra
             }
-        }, 20)
+        },duration)
     }
     let code = `
 /*
@@ -22,7 +42,7 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    background: #FEE433;
+    height:100%;
 }  
 .wrapper {
     width: 100%;
@@ -173,4 +193,5 @@
  */
 `
     writeCode('', code)
+   
 }.call()
